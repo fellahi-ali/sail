aa200dc2e75860e24e962962e430965a15d148ef
Colin Adler <cadler@coder.com>
2019-05-09 10:22:16 -0500

    asdflkj

--- globalflags.go
+++ globalflags.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"flag"
+	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
 
 	"github.com/fatih/color"
 	"go.coder.com/flog"
@@ -43,11 +47,47 @@ func requireRepo(conf config, prefs schemaPrefs, fl *flag.FlagSet) repo {
 		flog.Fatal("Argument <repo> must be provided.")
 	}
 
+	if pathIsRunnable(conf, repoURI) {
+
+	}
+
 	r, err := parseRepo(defaultSchema(conf, prefs), repoURI)
 	if err != nil {
 		flog.Fatal("failed to parse repo %q: %v", repoURI, err)
 	}
 	return r
+}
+
+func pathIsRunnable(conf config, path string) bool {
+	fp, err := filepath.Abs(path)
+	if err != nil {
+		return false
+	}
+
+	s, err := os.Stat(fp)
+	if err != nil {
+		return false
+	}
+
+	if !s.IsDir() {
+		return false
+	}
+
+	if match, _ := filepath.Match(expandRoot(conf.ProjectRoot), fp); !match {
+		return false
+	}
+
+	split := strings.Split(fp, "/")
+	if len(split) < 2 {
+		return false
+	}
+
+	return true
+}
+
+func expandRoot(path string) string {
+	u, _ := user.Current()
+	return strings.Replace(path, "~/", u.HomeDir+"/", 1)
 }
 
 func defaultSchema(conf config, prefs schemaPrefs) string {
